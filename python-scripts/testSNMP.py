d53e05a031c116b82f37df6db816c10e0f644e20
Rob Game <rgameuk@gmail.com>
2015-02-22 17:04:33 +0000

    Broken code into modules

--- python-scripts/testSNMP.py
+++ python-scripts/testSNMP.py
@@ -40,9 +40,7 @@ class cdpEntry(object):
 	def addSrcPort(self, srcPort):
 		self.srcPort = srcPort
 
-if __name__ == "__main__":
-	topology = routerList([])
-
+def getLocalCDPInfo(baseRouter):
 	cdpdiscovery = subprocess.check_output(["cdpr", "-d", "eth0"])
 	cdpList = string.split(cdpdiscovery,'\n')
 	cdpValues = [s for s in cdpList if re.search('value', s)]
@@ -50,60 +48,65 @@ if __name__ == "__main__":
 		cdpValues[idx] = val.replace(" ", "")
 	for idx, val in enumerate(cdpValues):
 		cdpValues[idx] = val.partition(':')[2]
-	baseRouter = router([])
 	baseRouter.addHostname(cdpValues[0])
 	baseRouter.addIpAddress(cdpValues[1])
-	topology.addRouter(baseRouter)
+	return baseRouter
 
-	print baseRouter.hostname
-	print baseRouter.ipAddress
-	hostnameOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", baseRouter.ipAddress, "1.3.6.1.4.1.9.9.23.1.2.1.1.6"])
+def getCDPHostnames(router):
+	hostnameOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", router.ipAddress, "1.3.6.1.4.1.9.9.23.1.2.1.1.6"])
 	hostnameOutput = string.split(hostnameOutput, '\n')
 	for idx, val in enumerate(hostnameOutput):
 		hostnameOutput[idx] = val.partition('"')[2]
 	for idx, val in enumerate(hostnameOutput):
 		hostnameOutput[idx] = val.replace('"', '')
 	hostnameOutput = filter(None, hostnameOutput)
-	print hostnameOutput
-	
-	ipAddressOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", baseRouter.ipAddress, "1.3.6.1.4.1.9.9.23.1.2.1.1.4"])
+	return hostnameOutput
+
+def getCDPIPs(router):
+	ipAddressOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", router.ipAddress, "1.3.6.1.4.1.9.9.23.1.2.1.1.4"])
 	ipAddressOutput = string.split(ipAddressOutput, '\n')
 	for idx, val in enumerate(ipAddressOutput):
 		ipAddressOutput[idx] = val.partition("STRING: ")[2]
 	for idx, val in enumerate(ipAddressOutput):
 		ipAddressOutput[idx] = val[:-1]
 	ipAddressOutput = filter(None, ipAddressOutput)
+	for idx, val in enumerate(ipAddressOutput):
+		firstOct = str(int(ipAddressOutput[idx][:2], 16))
+		secondOct = str(int(ipAddressOutput[idx][3:5], 16))
+		thirdOct = str(int(ipAddressOutput[idx][6:8], 16))
+		fourthOct = str(int(ipAddressOutput[idx][9:11], 16))
+		ipAddress = firstOct + '.' + secondOct + '.' + thirdOct + '.' + fourthOct
+		ipAddressOutput[idx] = ipAddress
+	return ipAddressOutput
 
-	firstOct = str(int(ipAddressOutput[0][:2], 16))
-	secondOct = str(int(ipAddressOutput[0][3:5], 16))
-	thirdOct = str(int(ipAddressOutput[0][6:8], 16))
-	fourthOct = str(int(ipAddressOutput[0][9:11], 16))
-	ipAddress = firstOct + '.' + secondOct + '.' + thirdOct + '.' + fourthOct
-	ipAddressOutput[0] = ipAddress
-
-	firstOct = str(int(ipAddressOutput[1][:2], 16))
-	secondOct = str(int(ipAddressOutput[1][3:5], 16))
-	thirdOct = str(int(ipAddressOutput[1][6:8], 16))
-	fourthOct = str(int(ipAddressOutput[1][9:11], 16))
-	ipAddress = firstOct + '.' + secondOct + '.' + thirdOct + '.' + fourthOct
-	ipAddressOutput[1] = ipAddress
-
-	print ipAddressOutput
-
-	interfaceOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", baseRouter.ipAddress, ".1.3.6.1.4.1.9.9.23.1.2.1.1.7"])
+def getCDPInterfaces(router):
+	interfaceOutput=subprocess.check_output(["snmpwalk", "-v", "2c", "-c", "public", router.ipAddress, ".1.3.6.1.4.1.9.9.23.1.2.1.1.7"])
 	interfaceOutput = string.split(interfaceOutput, '\n')
 	for idx, val in enumerate(interfaceOutput):
 		interfaceOutput[idx] = val.partition('"')[2]
 	for idx, val in enumerate(interfaceOutput):
 		interfaceOutput[idx] = val.replace('"', '')
 	interfaceOutput = filter(None, interfaceOutput)
-	print interfaceOutput
+	return interfaceOutput
+	
+if __name__ == "__main__":
+	topology = routerList([])
+	baseRouter = router([])
+	getLocalCDPInfo(baseRouter)	
+	hostnameList = getCDPHostnames(baseRouter)
+	IPAddressList = getCDPIPs(baseRouter)
+	InterfaceList = getCDPInterfaces(baseRouter)
 
-	for idx, val in enumerate(hostnameOutput):
-		newCDP = cdpEntry([])
-		newCDP.addHostname(hostnameOutput[idx])
-		newCDP.addIpAddress(ipAddressOutput[idx])
-		newCDP.addSrcPort(interfaceOutput[idx])
-		baseRouter.addCdpEntry(newCDP)
+	print hostnameList
+	print IPAddressList
+	print InterfaceList
 
-	print topology.routerList[0].cdp_entries[0].hostname
+	#for idx, val in enumerate(hostnameOutput):
+	#	newCDP = cdpEntry([])
+	#	newCDP.addHostname(hostnameOutput[idx])
+	#	newCDP.addIpAddress(ipAddressOutput[idx])
+	#	newCDP.addSrcPort(interfaceOutput[idx])
+	#	baseRouter.addCdpEntry(newCDP)
+
+	#print topology.routerList[0].cdp_entries[0].hostname
+
