1d7c012b16e9006b031e3b3bfe05ce24fa2cd9ef
Rob Game <Rob Game>
2015-03-04 22:56:26 +0000

    Interface replacement working, formatting into json next

--- python-scripts/NetworkDiscovery.py
+++ python-scripts/NetworkDiscovery.py
@@ -46,6 +46,13 @@ class cdpEntry(object):
 	def addDstPort(self, dstPort):
 		self.dstPort = dstPort
 
+class intChangeList(object):
+	def __init__(self, changeList):
+		self.changeList = []
+
+	def addChange(self, changeEntry):
+		self.changeList.append(changeEntry)
+
 class routerChanges(object):
 	def __init__(self, hostname):
 		self.hostname = ''
@@ -180,17 +187,31 @@ def updateRouters(updateRouter):
 			updateRouters(newRouter)
 
 def formatInterfaces(topology):
-	#Account for 0/0, serial, fastEthernet and 0/0/0 etc.
-	intChanges = []
+	#Account for 0/0/0 etc.
+	intChanges = intChangeList([])
 	for idx, val in enumerate(topology.routerList):
-		routerIntChanges = routerChanges([])
-		routerIntChanges.addHostname(val.hostname)
+		gigabitInts = []
+		fastEthernetInts = []
+		serialInts = []
 		interfaceValues = []
 		newValues = []
 		for cdpIDX, cdpVal in enumerate(val.cdp_entries):
-			#newInterface = cdpVal.srcPort.replace("Serial", "GigabitEthernet")
-			interfaceValues.append(cdpVal.srcPort)
-		interfaceValues.sort()
+			if 'Gigabit' in cdpVal.srcPort:
+				gigabitInts.append(cdpVal.srcPort)
+			elif 'FastEthernet' in cdpVal.srcPort:
+				fastEthernetInts.append(cdpVal.srcPort)
+			elif 'Serial' in cdpVal.srcPort:
+				serialInts.append(cdpVal.srcPort)
+			#interfaceValues.append(cdpVal.srcPort)
+		gigabitInts.sort()
+		fastEthernetInts.sort()
+		serialInts.sort()
+		if len(gigabitInts)>0:
+			interfaceValues = interfaceValues + gigabitInts
+		if len(fastEthernetInts)>0:
+			interfaceValues = interfaceValues + fastEthernetInts
+		if len(serialInts)>0:
+			interfaceValues = interfaceValues + serialInts
 		#TODO: Prioritise Gigabit interfaces over Fa and Se
 		# Idea: Three lists for each int type which is the combined?
 		localChanges = {}
@@ -198,18 +219,26 @@ def formatInterfaces(topology):
 			if interfaceValues[intIDX] in localChanges:
 				pass
 			else:
+				routerIntChanges = routerChanges([])
+				routerIntChanges.addHostname(val.hostname)
+				routerIntChanges.addOrignalPort(intVAL)
 				newInterface = "GigabitEthernet0/" + str(len(localChanges) + 1)
+				routerIntChanges.addNewPort(newInterface)
+				intChanges.addChange(routerIntChanges)
+				#print intChanges.changeList[0].hostname
+				print routerIntChanges.hostname + " was: " + routerIntChanges.orignalPort + " now: " + routerIntChanges.newPort
 				localChanges[intVAL] = newInterface
-		for val in enumerate(localChanges):
-			routerIntChanges.addOrignalPort(val)
-			#intNumb = re.findall(r'[0-9]/[0-9]+', interfaceValues[intIDX])
-			#try:
-			#	interfaceValues[intIDX] = (intNumb[intIDX])
-			#except:
-			#	pass
-			#print interfaceValues
-		#print interfaceValues
+		replaceInterfaces(topology, intChanges)
 
+def replaceInterfaces(topology, intChanges):
+	for i, changeEntry in enumerate(intChanges.changeList):
+		for j, routerVal in enumerate(topology.routerList):
+			for k, val in enumerate(routerVal.cdp_entries):
+				if routerVal.hostname == intChanges.changeList[i].hostname and val.srcPort == intChanges.changeList[i].orignalPort:
+					topology.routerList[j].cdp_entries[k].srcPort = intChanges.changeList[i].newPort
+				elif val.hostname == intChanges.changeList[i].hostname and val.dstPort == intChanges.changeList[i].orignalPort:
+					topology.routerList[j].cdp_entries[k].dstPort = intChanges.changeList[i].newPort
+		
 
 if __name__ == "__main__":
 	topology = routerList([])
@@ -233,11 +262,19 @@ if __name__ == "__main__":
 	#addRoutersFromCDP(baseRouter.cdp_entries)
 
 	updateRouters(baseRouter)
-	#for j, routerVal in enumerate(topology.routerList):
-	#	print routerVal.hostname + ":" + routerVal.ipAddress + ":" + routerVal.capabilities
-	#	for val in routerVal.cdp_entries:
-	#		print val.hostname + " on " + val.srcPort + " (local) " + val.dstPort + " (destination)"
-	#	print ""
+	for j, routerVal in enumerate(topology.routerList):
+		print routerVal.hostname + ":" + routerVal.ipAddress + ":" + routerVal.capabilities
+		for val in routerVal.cdp_entries:
+			print val.hostname + " on " + val.srcPort + " (local) " + val.dstPort + " (destination)"
+		print ""
 	pickle.dump(topology, open("topologyData.p", "wb"))
 
 	formatInterfaces(topology)
+
+	print "Formatted:"
+
+	for j, routerVal in enumerate(topology.routerList):
+		print routerVal.hostname + ":" + routerVal.ipAddress + ":" + routerVal.capabilities
+		for val in routerVal.cdp_entries:
+			print val.hostname + " on " + val.srcPort + " (local) " + val.dstPort + " (destination)"
+		print ""
