27c98bbbf0842b7cf45d6fd1898741cb5dc63504
Rob Game <Rob Game>
2015-04-06 22:47:57 +0100

    Changed location of download for VIRL file and added symlink in /var

--- python-scripts/downloadFiles.py
+++ python-scripts/downloadFiles.py
@@ -2,5 +2,5 @@ import pysftp
 
 with pysftp.Connection('178.62.24.178', username='sftp', private_key='/home/rob/.ssh/id_rsa') as sftp:
 	sftp.get_d('json-files', '/var/www/html')
-	sftp.get_d('virl-files', '/var/www/html')
-print 'Script has been run'
+	sftp.get_d('virl-files', '/home/rob/NetworkSimulation/python-scripts')
+print 'Files downloaded'
