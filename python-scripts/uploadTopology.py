143e197d3cde65bd3e4c8cd96a1801523024e1f0
Rob Game <Rob Game>
2015-04-10 15:33:16 +0100

    Server configured and allows access via management IP addresses

--- python-scripts/uploadTopology.py
+++ python-scripts/uploadTopology.py
@@ -1,22 +1,79 @@
+# coding=utf-8
 import os, requests, sys, json
 import telnetlib
+import time
 
-def updateServerIP(serverManIP, serverExtIP, virlHost, consolePort, username, password):
+
+def updateServerIP(serverManIP, serverExtIP, virlHost, consolePort, SNATAddress):
+	username = 'cisco'
+	password = 'cisco'
+	interfaceCommand = 'sudo ifconfig eth1 up '+ serverExtIP + ' netmask 255.255.255.0'
+	defaultGWCommand = 'sudo route add default gw 10.254.0.1 eth1'
+	nameserverOneCommand = 'sudo echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
+	nameserverTwoCommand = 'sudo echo "nameserver 8.8.4.4" >> /etc/resolv.conf'
 
 	telnet = telnetlib.Telnet(virlHost, int(consolePort))                   
 
 	telnet.write('\n')
 	response = telnet.read_until('virl-sim-server login:')
-	print response
-	telnet.write('cisco'+ '\n')
+	telnet.write(username+ '\n')
 	response = telnet.read_until('Password:')
-	print response
-	telnet.write('cisco' + '\n')
+	telnet.write(password + '\n')
 	telnet.write('\n')
 	response = telnet.read_until('cisco@virl-sim-server:~$')
-	telnet.write('touch telnet_test.file\n')
-	print response
+	telnet.write(str(interfaceCommand) + '\n')
+	response = telnet.read_until('[sudo] password for cisco:')
+	telnet.write(password + '\n')
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	telnet.write(defaultGWCommand + '\n')
+	#print response
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	#print response
+	telnet.write('sudo chmod 666 /etc/resolv.conf' + '\n')
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	#print response
+	telnet.write(nameserverOneCommand + '\n')
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	#print response
+	telnet.write(nameserverTwoCommand + '\n')
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	#print response
+	telnet.write('sudo chmod 644 /etc/resolv.conf' + '\n')
+	response = telnet.read_until('cisco@virl-sim-server:~$')
+	#print response
 	telnet.close()
+	print 'virl-sim-server interfaces updated. Server available @ ' + SNATAddress
+
+def addDeviceAccounts(virlHost, devicePortDict):
+	username = 'cisco'
+	password = 'cisco'
+	enablePassword = 'cisco'
+	accountCommand = 'username ' + username + ' password ' + password
+	enableCommand = 'enable password ' + enablePassword
+	
+	for key, val in devicePortDict.iteritems():
+		print key, val
+		telnet = telnetlib.Telnet(virlHost, val)             
+		telnet.write('\n')
+		response = telnet.read_until(key+'#')
+		print response
+		telnet.write('configure terminal'+ '\n')
+		response = telnet.read_until(key + '(config)#')
+		print response
+		telnet.write(accountCommand + '\n')
+		response = telnet.read_until(key + '(config)#')
+		telnet.write(enableCommand + '\n')
+		response = telnet.read_until(key + '(config)#')
+		telnet.write('end' + '\n')
+		response = telnet.read_until(key+'#')
+		print response
+		telnet.write('write memory' + '\n')
+		#response = telnet.read_until(key+'#')
+		#print response
+		telnet.close()
+	print 'Accounts added to devices'
+	print 'Username: ' + username
+	print 'Password: ' + password
 
 if __name__ == "__main__":
 	virlHost = "158.125.102.75"
@@ -28,16 +85,20 @@ if __name__ == "__main__":
 	headers = {'content-type': 'text/xml'}
 	payload = {'file': 'manual@launch_topo'}
 
-	#result = requests.post(url, auth=(username,password), params=payload, data=topology, headers=headers)
-	
+	result = requests.post(url, auth=(username,password), params=payload, data=topology, headers=headers)
+	print 'Waiting for simulation launch'
+	time.sleep(45) #Required as it takes time for ip addresses to be generated
+	print 'Gathering simulation information'
 	hostInformation = requests.get(hostsURL, auth=(username, password))
 	hostJSON = json.loads(hostInformation.text)
 	ipDictionary = {}
+	devicePortDict = {}
 	serverManIP = ''
 	serverExtIP = ''
 	SNATAddress = ''
 	lxcAddress = ''
-	consolePort = '' 
+	consolePort = ''
+	deviceConsolePort = '' 
 
 	#May have to investigate a time here as it takes ~ 10 seconds to spawn addresses
 	del hostJSON['UUID']
@@ -48,7 +109,11 @@ if __name__ == "__main__":
 			if ((devKey == 'NodeSubtype') and (devVal == 'IOSv')):
 				hostname = rawDeviceInfo['NodeName']
 				managementIP = rawDeviceInfo['managementIP']
-				ipDictionary[hostname] = managementIP
+				deviceConsolePort = rawDeviceInfo['managementIP']
+				ipDictionary[str(hostname)] = str(managementIP)
+				deviceConsolePort = rawDeviceInfo['PortConsole']
+				print deviceConsolePort
+				devicePortDict[str(hostname)] = int(deviceConsolePort)
 			if ((devKey == 'NodeName') and (devVal == 'virl-sim-server')):
 				serverManIP = rawDeviceInfo['managementIP']
 				serverExtIP = rawDeviceInfo['internalAddr']
@@ -57,4 +122,6 @@ if __name__ == "__main__":
 			if 'lxc-flat.External Address' in key:
 				if (devKey == 'managementIP'):
 					lxcAddress = devVal
-	updateServerIP(serverManIP, serverExtIP, virlHost, consolePort, username, password)
+	print devicePortDict
+	updateServerIP(serverManIP, serverExtIP, virlHost, consolePort, SNATAddress)
+	#addDeviceAccounts(virlHost, devicePortDict)
