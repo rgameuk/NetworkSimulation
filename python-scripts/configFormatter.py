567be2278f6a3d79bcc4bc603c3b935759d1e833
Rob Game <Rob Game>
2015-04-14 21:28:51 +0100

    Fixing list issue in config splitter

--- python-scripts/configFormatter.py
+++ python-scripts/configFormatter.py
@@ -43,66 +43,65 @@ if __name__ == "__main__":
 		for key, value in routerList.iteritems():
 			shutil.copy('/var/lib/rancid/Discovered/configs/' + key.lower(), configLocation)
 	
-	#try:
-	for key, value in routerList.iteritems(): 
-		with open('./config-files/' + key.lower(), 'r') as f:
-			config = f.read()
-			f.close()
-		oldConfig = config.partition('version')
-		configList = [oldConfig[i] for i in (1, 2)]
-		config = configList[0] + configList[1]
-		print config
-		with open('./config-files/' + key.lower() + '.short', 'w') as f:
-			f.write(config)
+	try:
+		for key, value in routerList.iteritems(): 
+			with open('./config-files/' + key.lower(), 'r') as f:
+				config = f.read()
+				f.close()
+			oldConfig = config.partition('version')
+			configList = [oldConfig[i] for i in (1, 2)]
+			config = configList[0] + configList[1]
+			with open('./config-files/' + key.lower() + '.short', 'w') as f:
+				f.write(config)
 
-		p = CiscoConfParse('./config-files/' + key.lower() + '.short')
+			p = CiscoConfParse('./config-files/' + key.lower() + '.short')
 
-		for i, changeEntry in enumerate(interfaceChanges.changeList):
-			oldPort = changeEntry.orignalPort
-			if changeEntry.hostname == key:
-				if 'Serial' in changeEntry.orignalPort:
-					obj = p.find_objects(changeEntry.orignalPort)
-					print obj
-					if (obj[0].has_child_with(r' bandwidth') == False):
-						obj[0].append_to_family(' bandwidth 2000')
-				if 'FastEthernet' in changeEntry.orignalPort:
-					obj = p.find_objects(changeEntry.orignalPort)
-					print obj
-					if (obj[0].has_child_with(r' bandwidth') == False):
-						obj[0].append_to_family(' bandwidth 100000')
-				p.replace_lines(changeEntry.orignalPort, changeEntry.newPort)
+			for i, changeEntry in enumerate(interfaceChanges.changeList):
+				oldPort = changeEntry.orignalPort
+				if changeEntry.hostname == key:
+					if 'Serial' in changeEntry.orignalPort:
+						obj = p.find_objects(changeEntry.orignalPort)
+						print obj
+						if (obj[0].has_child_with(r' bandwidth') == False):
+							obj[0].append_to_family(' bandwidth 2000')
+					if 'FastEthernet' in changeEntry.orignalPort:
+						obj = p.find_objects(changeEntry.orignalPort)
+						print obj
+						if (obj[0].has_child_with(r' bandwidth') == False):
+							obj[0].append_to_family(' bandwidth 100000')
+					p.replace_lines(changeEntry.orignalPort, changeEntry.newPort)
 
 
-		for obj in p.find_objects(r'Serial'):
-			obj.delete(recurse=True)
+			for obj in p.find_objects(r'Serial'):
+				obj.delete(recurse=True)
 
-		for obj in p.find_objects(r'FastEthernet'):
-			obj.delete(recurse=True)
+			for obj in p.find_objects(r'FastEthernet'):
+				obj.delete(recurse=True)
 
-		for obj in p.find_objects(r'^interface'):
-			obj.delete_children_matching(r'clock rate')
+			for obj in p.find_objects(r'^interface'):
+				obj.delete_children_matching(r'clock rate')
 
-		p.insert_before('interface GigabitEthernet0/1', 'interface GigabitEthernet0/0', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', '!', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', ' no shutdown', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', ' speed auto', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', ' duplex auto', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', ' no ip address', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', '! Configured on launch', exactmatch=True, atomic=False)
-		p.insert_after('interface GigabitEthernet0/0', ' description OOB Management', exactmatch=True, atomic=False)
+			p.insert_before('interface GigabitEthernet0/1', 'interface GigabitEthernet0/0', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', '!', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', ' no shutdown', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', ' speed auto', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', ' duplex auto', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', ' no ip address', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', '! Configured on launch', exactmatch=True, atomic=False)
+			p.insert_after('interface GigabitEthernet0/0', ' description OOB Management', exactmatch=True, atomic=False)
 
-		for obj in p.find_objects(r'^line vty'):
-			obj.delete(recurse=True)
-		for obj in p.find_objects(r'^enable password'):
-			obj.delete()
-		for obj in p.find_objects(r'^login password'):
-			obj.delete()
+			for obj in p.find_objects(r'^line vty'):
+				obj.delete(recurse=True)
+			for obj in p.find_objects(r'^enable password'):
+				obj.delete()
+			for obj in p.find_objects(r'^login password'):
+				obj.delete()
 
-		p.insert_before('end', 'line vty 0 4', exactmatch=True, atomic=False)
-		p.insert_after('line vty 0 4', ' no login', exactmatch=True, atomic=False)
-		p.insert_after(' no login', ' privilege level 15', exactmatch=True, atomic=False)
-		p.insert_after('line vty 0 4', ' transport input telnet ssh', exactmatch=True, atomic=False)
+			p.insert_before('end', 'line vty 0 4', exactmatch=True, atomic=False)
+			p.insert_after('line vty 0 4', ' no login', exactmatch=True, atomic=False)
+			p.insert_after(' no login', ' privilege level 15', exactmatch=True, atomic=False)
+			p.insert_after('line vty 0 4', ' transport input telnet ssh', exactmatch=True, atomic=False)
 
-		p.save_as('./config-files/' + key.lower() + '.edited')
-	#except:
-	#	print key + ' is not a valid Cisco configuration file. Aborting configuration edits'
+			p.save_as('./config-files/' + key.lower() + '.edited')
+	except:
+		print key + ' is not a valid Cisco configuration file. Aborting configuration edits'
